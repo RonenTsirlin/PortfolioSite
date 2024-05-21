3c7f332122921d877fa1665b46f1214d90b0b3f0
RonenTsirlin <Ronentz123@gmail.com>
2024-05-21 18:57:39 +0300

    Added the backend url to the js script.

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const formData = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-    fetch('/contact', {  // This will send the request to the Flask backend
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.status);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    try {
+        const response = await fetch('https://portfoliosite-omhc.onrender.com/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, message }),
+        });
+
+        if (response.ok) {
+            alert('Email sent successfully');
+        } else {
+            alert('Failed to send email');
+        }
+    } catch (error) {
+        alert('Error: ' + error.message);
+    }
 });
